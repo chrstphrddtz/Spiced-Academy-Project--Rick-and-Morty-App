7a80cf35329e12321b188c578b501cd5c564e648
DusanRajcevic1989 <dule.rajca@gmail.com>
2023-03-14 13:26:20 +0100

    add functionality to cars.js

--- components/card/card.js
+++ components/card/card.js
@@ -1,27 +1,29 @@
-export function createCharacterCard() {
-    const li = document.createElement("li")
-    li.classList.add("card")
-    li.innerHTML = `
+// import { fetchCharacters } from "../../index.js";
+
+export function createCharacterCard(cardItem) {
+  const li = document.createElement("li");
+  li.classList.add("card");
+  li.innerHTML = `
         <div class="card__image-container">
         <img
             class="card__image"
-            src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+            src="${cardItem.image}"
             alt="Rick Sanchez"
         />
         <div class="card__image-gradient"></div>
         </div>
         <div class="card__content">
-        <h2 class="card__title">Rick Sanchez</h2>
+        <h2 class="card__title">${cardItem.name}</h2>
         <dl class="card__info">
             <dt class="card__info-title">Status</dt>
-            <dd class="card__info-description">Alive</dd>
+            <dd class="card__info-description">${cardItem.status}</dd>
             <dt class="card__info-title">Type</dt>
-            <dd class="card__info-description"></dd>
+            <dd class="card__info-description">${cardItem.type}</dd>
             <dt class="card__info-title">Occurrences</dt>
-            <dd class="card__info-description">51</dd>
+            <dd class="card__info-description">${cardItem.episode.length}</dd>
         </dl>
         </div>
         
-    `
-    return li
+    `;
+  return li;
 }
